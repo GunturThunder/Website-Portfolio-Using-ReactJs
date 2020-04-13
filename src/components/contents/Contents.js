2018e6b9ea4cc8a3affc8f61aeb73bd3fb3e8cdf
GunturThunder <gangkasa888@gmail.com>
2020-04-13 13:23:14 +0700

    first commit

---
+++ src/components/contents/Contents.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './main.css'
+
+class Contents extends Component {
+    render(){
+        return(
+            <div className="wrap">
+                <aside className="sidebar1">
+
+                </aside>
+                <aside className="sidebar2">
+
+                </aside>
+            </div>
+        )
+    }
+}
+
+export default Contents
---
+++ src/components/home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Contents from '../contents/Contents'
+
+class Home extends Component {
+    render(){
+        return(
+            <div className="wrap">
+                <Contents /> 
+            </div>
+        )
+    }
+}
+export default Home
